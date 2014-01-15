762b06579a0ede89fd00469ccc67e106e4692afc
Chase McCarthy <chase@code0100fun.com>
2014-01-14 20:05:07 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+
+var express = require('express')
+    , app = express()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server);
+
+server.listen(9000);
+
+app.use('/', express.static(__dirname + '/../dist/'));
+
+console.log(__dirname + '/../dist/');
+var rooms = {};
+io.sockets.on('connection', function (socket) {
+
+    socket.on('mousemove', function (data) {
+
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('moving', data);
+    });
+});
